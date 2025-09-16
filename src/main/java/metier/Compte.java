7bd1eaa754fe81018e8117134cc2445f240c7a91
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-16 16:22:08 +0100

    Implémenter classe Abstract Compte

--- src/main/java/metier/Compte.java
+++ src/main/java/metier/Compte.java
@@ -1,5 +1,17 @@
 package main.java.metier;
+import java.util.ArrayList;
+import java.util.List;
+public abstract class Compte {
+    protected String code;
+    protected double solde;
+    protected List<Operation> listeOperations;
 
-public class Compte {
-    
+    public Compte(String code , double solde ){
+        this.code = code;
+        this.solde = solde ;
+        this.listeOperations = new ArrayList<>();
+    }
+    public abstract void  retiret(double montant);
+    public abstract double  calculerIntret();
+    public abstract void  afficherDetails();
 }
