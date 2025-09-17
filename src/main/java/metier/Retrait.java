f683046effbb50c79a1f15b2ddab9b0c577d7ed1
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-17 11:26:30 +0100

    Implémenter Class Retrait

--- src/main/java/metier/Retrait.java
+++ src/main/java/metier/Retrait.java
@@ -1,5 +1,21 @@
 package main.java.metier;
 
-public class Retrait {
-    
+public class Retrait extends Operation {
+    private String destination;
+    public Retrait(String id, double montant ,String destination) {
+        super(id, montant);
+        this.destination = destination;
+    }
+
+    public   String getDestination() {
+        return destination;
+    }
+    @Override
+    public String getType() {
+        return "Retrait";
+    }
+//    @Override
+//    public String toString() {
+//        return "Retrait de " + montant + "€ - Destination: " + destination + " - " + DateUtil.formatDate(date);
+//    }
 }
