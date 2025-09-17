283116b4ff41e043c4697da57fdb812b67f3509e
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-17 11:15:43 +0100

    Implémenter Class Operation

--- src/main/java/metier/Operation.java
+++ src/main/java/metier/Operation.java
@@ -1,5 +1,31 @@
 package main.java.metier;
+import java.time.LocalDateTime;
+import util.DateUtil;
 
-public class Operation {
-    
+public abstract class Operation {
+    protected String id;
+    protected LocalDateTime  date;
+    protected double montant;
+
+    public Operation(String id, double montant) {
+        this.id = id;
+        this.montant = montant;
+        this.date = LocalDateTime.now();
+    }
+    public String getId() {
+        return id;
+    }
+    public double getMontant() {
+        return montant;
+    }
+    public LocalDateTime getDate() {
+        return date;
+    }
+    public abstract String getType();
+//    @Override
+//
+//        public String toString() {
+//            return getType() + " de " + montant + "€ - " + DateUtil.formatDate(date);
+//        }
+
 }
