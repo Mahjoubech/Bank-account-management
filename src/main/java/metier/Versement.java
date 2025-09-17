a2ce2025a200dfb4518d9f103e2c1d8c5b5a0dd9
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-17 11:26:10 +0100

    Implémenter Class Versement

--- src/main/java/metier/Versement.java
+++ src/main/java/metier/Versement.java
@@ -1,5 +1,20 @@
 package main.java.metier;
 
-public class Versement {
-    
+public class Versement extends Operation{
+    private String source ;
+    public Versement(String id, double montant ,String source){
+        super(id,montant);
+        this.source = source;
+    }
+    public String getSource() {
+        return source;
+    }
+    @Override
+    public String getType() {
+        return "Versement";
+    }
+//    @Override
+//    public String toString() {
+//        return "Versement de " + montant + "€ - Source: " + source + " - " + DateUtil.formatDate(date);
+//    }
 }
