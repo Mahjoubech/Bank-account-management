9447c23cc51d3aa08621f088f82e4888a7072391
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-19 16:29:01 +0100

    https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SCRUM-21

--- src/main/java/metier/CompteEpargne.java
+++ src/main/java/metier/CompteEpargne.java
@@ -1,32 +1,28 @@
 package metier;
 
+import util.Validateur;
+
 public class CompteEpargne extends Compte {
     private double tauxInteret;
-
     public CompteEpargne(String code, double solde, double tauxInteret) {
         super(code, solde);
+        if (!Validateur.isPositiveDouble(tauxInteret)) throw new IllegalArgumentException("Taux doit être positif !");
         this.tauxInteret = tauxInteret;
     }
-
+    public double getTauxInteret() { return tauxInteret; }
     @Override
     public void retirer(double montant, String destination) throws Exception {
-        if (solde < montant) {
-            throw new Exception("Solde insuffisant !");
-        }
-        ajouterOperation(new Retrait(montant, destination));
+        if (!Validateur.isPositiveAmount(montant)) throw new Exception("Montant négatif ou nul !");
+        if (solde < montant) throw new Exception("Solde insuffisant !");
+        ajouterOperation(new metier.Retrait(montant, destination));
+        solde -= montant;
     }
-
     @Override
-    public double calculerInteret() {
-        return solde * tauxInteret / 100.0;
-    }
-
+    public double calculerInteret() { return solde * tauxInteret / 100.0; }
     @Override
     public void afficherDetails() {
-        System.out.println("\nCompte Epargne [" + code + "]");
-        System.out.println("Solde: " + solde + " EUR, Taux: " + tauxInteret + "%");
-        for (Operation op : listeOperations) {
-            System.out.println(op);
-        }
+        System.out.println(util.ConsoleColor.colorize(util.ConsoleColor.MAGENTA, "CompteEpargne [" + code + "] Solde: " + solde + " Taux: " + tauxInteret + "%"));
+        System.out.println(util.ConsoleColor.colorize(util.ConsoleColor.CYAN, "Historique opérations:"));
+        listeOperations.forEach(op -> System.out.println(util.ConsoleColor.colorize(util.ConsoleColor.YELLOW, op.toString())));
     }
 }
