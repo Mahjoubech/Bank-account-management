2af5d9a3b1be7effeebfc8231773eadc2802262e
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-19 16:35:35 +0100

    Effectuer un retrait : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SCRUM-42

--- src/main/java/metier/Retrait.java
+++ src/main/java/metier/Retrait.java
@@ -2,14 +2,12 @@ package metier;
 
 public class Retrait extends Operation {
     private String destination;
-
     public Retrait(double montant, String destination) {
         super(montant);
         this.destination = destination;
     }
-
     @Override
-    public String toString() {
-        return super.toString() + " (Retrait, Dest: " + destination + ")";
-    }
+    public String getType() { return "Retrait"; }
+    @Override
+    public String getDetails() { return "Destination: " + destination; }
 }
