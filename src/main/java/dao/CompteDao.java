38d742ba0b1e01ff24997483be33e9f8b574a231
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-19 16:20:49 +0100

    https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SCRUM-67

--- src/main/java/dao/CompteDao.java
+++ src/main/java/dao/CompteDao.java
@@ -1,5 +1,77 @@
-package main.java.dao;
+package dao;
+
+import metier.Compte;
+import metier.CompteCourant;
+import metier.CompteEpargne;
+import util.ConsoleColor;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CompteDao {
-    
+    private Connection cnx;
+
+    public CompteDao(Connection cnx) {
+        this.cnx = cnx;
+    }
+
+    public void save(Compte compte) throws SQLException {
+        String sql = "INSERT INTO compte (code, type, solde, decouvert, tauxInteret) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement ps = cnx.prepareStatement(sql)) {
+            ps.setString(1, compte.getCode());
+            ps.setString(2, (compte instanceof CompteCourant) ? "courant" : "epargne");
+            ps.setDouble(3, compte.getSolde());
+            ps.setDouble(4, (compte instanceof CompteCourant) ? ((CompteCourant)compte).getDecouvert() : 0);
+            ps.setDouble(5, (compte instanceof CompteEpargne) ? ((CompteEpargne)compte).getTauxInteret() : 0);
+            ps.executeUpdate();
+            System.out.println(ConsoleColor.colorize(ConsoleColor.GREEN, "Compte enregistré en base!"));
+        }
+    }
+
+    public List<Compte> findAll() throws SQLException {
+        List<Compte> list = new ArrayList<>();
+        String sql = "SELECT * FROM compte";
+        try (Statement st = cnx.createStatement(); ResultSet rs = st.executeQuery(sql)) {
+            while (rs.next()) {
+                String code = rs.getString("code");
+                double solde = rs.getDouble("solde");
+                String type = rs.getString("type");
+                double decouvert = rs.getDouble("decouvert");
+                double tauxInteret = rs.getDouble("tauxInteret");
+                if ("courant".equals(type))
+                    list.add(new CompteCourant(code, solde, decouvert));
+                else
+                    list.add(new CompteEpargne(code, solde, tauxInteret));
+            }
+        }
+        return list;
+    }
+
+    public Compte findByCode(String code) throws SQLException {
+        String sql = "SELECT * FROM compte WHERE code = ?";
+        try (PreparedStatement ps = cnx.prepareStatement(sql)) {
+            ps.setString(1, code);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    String type = rs.getString("type");
+                    double solde = rs.getDouble("solde");
+                    double decouvert = rs.getDouble("decouvert");
+                    double tauxInteret = rs.getDouble("tauxInteret");
+                    if ("courant".equals(type))
+                        return new CompteCourant(code, solde, decouvert);
+                    else
+                        return new CompteEpargne(code, solde, tauxInteret);
+                }
+            }
+        }
+        return null;
+    }
+    public void updateSolde(String codeCompte, double nouveauSolde) throws SQLException {
+        String sql = "UPDATE compte SET solde = ? WHERE code = ?";
+        try (PreparedStatement ps = cnx.prepareStatement(sql)) {
+            ps.setDouble(1, nouveauSolde);
+            ps.setString(2, codeCompte);
+            ps.executeUpdate();
+        }
+    }
 }
--- src/main/java/dao/OperationDao.java
+++ src/main/java/dao/OperationDao.java
@@ -1,5 +1,60 @@
-package main.java.dao;
+package dao;
+
+import metier.Operation;
+import metier.Versement;
+import metier.Retrait;
+import util.ConsoleColor;
+import util.DateUtil;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OperationDao {
-    
+    private Connection cnx;
+
+    public OperationDao(Connection cnx) {
+        this.cnx = cnx;
+    }
+
+    public void save(Operation op, String codeCompte) throws SQLException {
+        String sql = "INSERT INTO operation (numero, code_compte, type, montant, date, details) VALUES (?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement ps = cnx.prepareStatement(sql)) {
+            ps.setString(1, op.getNumero());
+            ps.setString(2, codeCompte);
+            ps.setString(3, op.getType());
+            ps.setDouble(4, op.getMontant());
+            ps.setString(5, DateUtil.format(op.getDate()));
+            ps.setString(6, op.getDetails());
+            ps.executeUpdate();
+            System.out.println(ConsoleColor.colorize(ConsoleColor.GREEN, "Operation enregistrée en base!"));
+        }
+    }
+
+    public List<Operation> findByCompte(String codeCompte) throws SQLException {
+        List<Operation> list = new ArrayList<>();
+        String sql = "SELECT * FROM operation WHERE code_compte = ?";
+        try (PreparedStatement ps = cnx.prepareStatement(sql)) {
+            ps.setString(1, codeCompte);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    String numero = rs.getString("numero");
+                    String type = rs.getString("type");
+                    double montant = rs.getDouble("montant");
+                    String dateStr = rs.getString("date");
+                    String details = rs.getString("details");
+                    LocalDateTime date = LocalDateTime.parse(dateStr, java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                    Operation op = null;
+                    if ("Versement".equalsIgnoreCase(type))
+                        op = new Versement(montant, details);
+                    else
+                        op = new Retrait(montant, details);
+                    op.setNumero(numero);
+                    op.setDate(date);
+                    list.add(op);
+                }
+            }
+        }
+        return list;
+    }
 }
