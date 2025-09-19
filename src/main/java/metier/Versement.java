b577653e6f56fc4b0ed0bcf651710dbcd7702256
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-19 16:30:20 +0100

    https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SCRUM-46

--- src/main/java/metier/Versement.java
+++ src/main/java/metier/Versement.java
@@ -2,14 +2,12 @@ package metier;
 
 public class Versement extends Operation {
     private String source;
-
     public Versement(double montant, String source) {
         super(montant);
         this.source = source;
     }
-
     @Override
-    public String toString() {
-        return super.toString() + " (Versement, Source: " + source + ")";
-    }
+    public String getType() { return "Versement"; }
+    @Override
+    public String getDetails() { return "Source: " + source; }
 }
