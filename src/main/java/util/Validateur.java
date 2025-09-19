d82f57943c7584834d9a041846e7afb51f0d3898
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-19 16:10:54 +0100

    Vérifier montants positifs

--- src/main/java/util/Validateur.java
+++ src/main/java/util/Validateur.java
@@ -1,4 +1,13 @@
-package main.java.util;
+package util;
 
 public class Validateur {
+    public static boolean isValidCompteCode(String code) {
+        return code != null && code.matches("CPT-\\d{5}");
+    }
+    public static boolean isPositiveAmount(double montant) {
+        return montant > 0;
+    }
+    public static boolean isPositiveDouble(double value) {
+        return value >= 0;
+    }
 }
