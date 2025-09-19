0ffd35a5965e32a58ade730faeb548edc404a96c
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-19 16:32:55 +0100

    https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SCRUM-68

--- src/main/java/metier/Operation.java
+++ src/main/java/metier/Operation.java
@@ -1,25 +1,28 @@
 package metier;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
+import util.DateUtil;
 
 public abstract class Operation {
     protected String numero;
-    protected LocalDate date;
+    protected LocalDateTime date;
     protected double montant;
 
     public Operation(double montant) {
         this.numero = UUID.randomUUID().toString();
-        this.date = LocalDate.now();
+        this.date = DateUtil.now();
         this.montant = montant;
     }
-
     public String getNumero() { return numero; }
-    public LocalDate getDate() { return date; }
+    public LocalDateTime getDate() { return date; }
     public double getMontant() { return montant; }
-
+    public abstract String getType();
+    public abstract String getDetails();
+    public void setNumero(String numero) { this.numero = numero; }
+    public void setDate(LocalDateTime date) { this.date = date; }
     @Override
     public String toString() {
-        return "[" + date + "] #" + numero + " : " + montant + " EUR";
+        return "[" + DateUtil.format(date) + "] #" + numero + " : " + montant + " DH (" + getType() + ", " + getDetails() + ")";
     }
 }
